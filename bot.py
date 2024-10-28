78112b22ac5638399bdc9bdc2222995091d7861c
Nim12334 <ninlin6859@gmail.com>
2024-10-28 21:25:37 +0300

    Add files via upload
    простая нейросеть на numpy

---
+++ bot.py
@@ -0,0 +1,12 @@
+import numpy as np
+#входные данные
+
+a=np.random.random(12)
+b=np.random.random(12)
+c=np.random.random(12)
+print("Вхадные данные:",a,b,c)
+#оброботка данных
+a1=np.array([a,b,c])
+c1=np.mean(a)
+#выходные данные
+print("Выходные даные(среднее значение):",c1и)
